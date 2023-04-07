755ed5634fc356e6d11448b7d59e3829a26ca20a
sergkhr <serg.khrechko@gmail.com>
2023-04-07 12:37:54 +0300

    yaaay trees work

--- Workspace/Third Practice/trees.py
+++ Workspace/Third Practice/trees.py
@@ -4,100 +4,107 @@ from matplotlib.collections import LineCollection
 
 # settings
 x_field_size = 100
-y_field_size = 100
-attraction_count = 100
-attraction_radius = 50
-attraction_death_radius = 2
-growth_distance = 2
+y_field_size = 200
+attraction_count = 150
+attraction_radius = 1000  # heeeh big number
+attraction_death_radius = 5
+growth_distance = 3
 
-# (x, y), (dx, dy) - starting point and direction of growth
-growth_starting_point = np.array([np.array([50, 5]), np.array([0, 0])])
-# Create a random set of attractors
-attractors = np.random.rand(attraction_count, 2) * [x_field_size, y_field_size]
-#print(attractors)
+# for drawing
+lines = []
+leaves = []
+
+# [x, y], [list of active_attractors] - starting point and direction of growth
+growth_starting_point = [np.array([x_field_size / 2, 0]), []]
+# Create a random set of attractors (from 0 to x_field_size, from 50 to y_field_size)
+attractors = np.random.rand(attraction_count, 2) * [x_field_size, y_field_size / 2] + [0, y_field_size / 2]
+# print(attractors)
 # # draw the attractors as a scatter plot
-#plt.scatter(attractors[:, 0], attractors[:, 1], s=1, c='k')
+fig, ax = plt.subplots()
+plt.scatter(attractors[:, 0], attractors[:, 1], s=1, c='k')
 # plt.show()
 
-tree = np.array([growth_starting_point])
+tree = [growth_starting_point]
 
 
-def attract_closest(attractor_point, tree, attractors):
-    # get the closest point on the tree to the attractor
+def attract_closest(attractor_point):
+    global tree
+    global attractors
+    # get the index of the closest point to the attractor, search only by x and y
     closest_point_index = 0
     for i in range(len(tree)):
-        if np.linalg.norm(np.array(tree[i][0]) - np.array(attractor_point)) < \
-                    np.linalg.norm(np.array(tree[closest_point_index][0]) - np.array(attractor_point)):
+        # print(i, tree)
+        # print(tree[i][0], attractor_point)
+        if np.linalg.norm(np.array(tree[i][0]) - np.array(attractor_point)) < np.linalg.norm(np.array(tree[closest_point_index][0]) - np.array(attractor_point)):
             closest_point_index = i
-    closest_point = tree[closest_point_index]
+    closest_point_cords = tree[closest_point_index][0]
     # get the distance between the attractor and the closest point
-    distance = np.linalg.norm(np.array(closest_point[0]) - np.array(attractor_point))
+    distance = np.linalg.norm(np.array(closest_point_cords) - np.array(attractor_point))
     # print(attractor_point, closest_point[0], distance)
-    
     if distance < attraction_death_radius:
         # find attractor_point index in attractors
         ind = np.where(attractors == attractor_point)[0][0]
         # remove the attractor from the list
         attractors = np.delete(attractors, ind, 0)
-
-    #print(distance, attraction_radius)
-    if distance < attraction_radius:
-        #print('Attracting...')
-        temp_dx = attractor_point[0] - closest_point[0][0]
-        temp_dy = attractor_point[1] - closest_point[0][1]
-        dx = temp_dx / distance
-        dy = temp_dy / distance
-        # set the direction of growth of the closest point to the direction of the attractor
-        new_dx = closest_point[1][0] + dx
-        new_dy = closest_point[1][1] + dy
-        tmp_distance = np.linalg.norm(np.array([new_dx, new_dy]))
-        tmp_cls_point = np.array([closest_point[0], np.array([new_dx / tmp_distance, new_dy / tmp_distance])])
-        tree[closest_point_index] = tmp_cls_point.copy()
-        print (tmp_cls_point)
-        print(tree[closest_point_index])
+        # make closest point a leaf
+        leaves.append(closest_point_cords)
+    elif distance < attraction_radius:
+        # add the attractor to the list of active attractors
+        tree[closest_point_index][1].append(attractor_point)
 
 
+def grow_tree(point):
+    global tree
+    global lines
+    normalized_direction = [0, 0]
+    # iterate through all attractors and find those that are within the attraction radius
+    for attractor in point[1]:
+        normalized_direction += (attractor - point[0]) / (np.linalg.norm(attractor - point[0])**2)  # squared distance
+        # to make the closer attractors more important
 
-def grow_tree(tree):
-    for point in tree:
-        #print(point)
-        if point[1][0] != 0 or point[1][1] != 0:
-            print('Growing tree...')
-            # add the direction of growth to the point, work with numpy ndarray
-            new_point = (np.array(point[0]) + np.array(point[1]) * growth_distance, np.array([0, 0]))
-            # add the new point to the tree
-            tree.append((new_point, point[1]))
-    return tree
+    if len(point[1]) > 0:
+        # normalize the direction
+        normalized_direction = normalized_direction / np.linalg.norm(normalized_direction)
+        # add the new point to the tree
+        new_point = [point[0] + normalized_direction * growth_distance, []]
+        tree.append(new_point)
+        # add the line to the lines list
+        lines.append([point[0], new_point[0]])
+        # remove the attractors from the list of active attractors
+        point[1] = []
 
 
-def grow_iteration(tree, attractors):
+def grow_iteration():
+    global tree
+    global attractors
     # attract the point of the tree to the attractors
     for point in attractors:
-        #print(tree)
-        attract_closest(point, tree, attractors)
-        #print(tree)
-
+        attract_closest(point)
     # grow the tree
-    tree = grow_tree(tree)
-    return tree
+    for point in tree:
+        grow_tree(point)
 
 
 # while there are still attractors or 100 iterations have not been reached
 iter_count = 0
-while len(attractors) > 0 and iter_count < 10:
-    tree = grow_iteration(tree, attractors)
+while len(attractors) > 0 and iter_count < 200:
+    print(iter_count, len(attractors), len(tree))
+    grow_iteration()
     iter_count += 1
 
 print(len(tree))
 print(iter_count)
 
 # drawing the tree
-lines = []
-for i in range(len(tree) - 1):
-    lines.append([tree[i][0], tree[i + 1][0]])
 lc = LineCollection(lines, linewidths=1)
-fig, ax = plt.subplots()
+# draw leaves as a scatter plot with circles
+leaf_radius = attraction_death_radius
+for leaf in leaves:
+    circle = plt.Circle(leaf, leaf_radius, alpha=0.5, color='g')
+    ax.add_patch(circle)
+
 ax.add_collection(lc)
-ax.autoscale()
-ax.margins(0.1)
+ax.set_aspect('equal')
+ax.set_xlim(-10, x_field_size + 10)
+ax.set_ylim(-10, y_field_size + 10)
 plt.show()
