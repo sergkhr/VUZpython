29a6bd9db554e77e75c61069bb2c341aba1a091a
sergkhr <serg.khrechko@gmail.com>
2023-05-23 18:45:29 +0300

    eleventh done

--- Workspace/eleventh.py
+++ Workspace/eleventh.py
@@ -37,9 +37,38 @@ def parse_a(buf, offset):
 
 
 def parse_b(buf, offset):
-    a1 = []
+    b1 = ''
     for _ in range(7):
-        
+        value, offset = parse(buf, offset, 'char')
+        b1 += value.decode('utf-8')
+    offset_b2, offset = parse(buf, offset, 'uint32')
+    b2, _ = parse_c(buf, offset_b2)
+    size_b3, offset = parse(buf, offset, 'uint16')
+    offset_b3, offset = parse(buf, offset, 'uint16')
+    b3 = []
+    for _ in range(size_b3):
+        value, offset_b3 = parse_d(buf, offset_b3)
+        b3.append(value)
+    b4, offset = parse(buf, offset, 'uint64')
+    b5 = []
+    for _ in range(6):
+        value, offset = parse(buf, offset, 'double')
+        b5.append(value)
+    b6, offset = parse(buf, offset, 'int32')
+    return dict(B1=b1, B2=b2, B3=b3, B4=b4, B5=b5, B6=b6), offset
+
+
+def parse_c(buf, offset):
+    c1, offset = parse(buf, offset, 'double')
+    c2, offset = parse(buf, offset, 'int64')
+    c3, offset = parse(buf, offset, 'int32')
+    return dict(C1=c1, C2=c2, C3=c3), offset
+
+
+def parse_d(buf, offset):
+    d1, offset = parse(buf, offset, 'double')
+    d2, offset = parse(buf, offset, 'int8')
+    return dict(D1=d1, D2=d2), offset
 
 
 def main(stream):
